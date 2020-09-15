709b28001433ba16467b020582e77c8d04de03c2
Minamiguchi Haruhiko <haruhikoM@gmail.com>
2020-09-15 18:10:15 +0900

    Initial Commit

---
+++ RayTracerChallengeTests/RayTracerChallengeTests.swift
@@ -0,0 +1,34 @@
+//
+//  RayTracerChallengeTests.swift
+//  RayTracerChallengeTests
+//
+//  Created by Minamiguchi Haruhiko on 2020/09/15.
+//  Copyright © 2020 Minamiguchi Haruhiko. All rights reserved.
+//
+
+import XCTest
+@testable import RayTracerChallenge
+
+class RayTracerChallengeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
